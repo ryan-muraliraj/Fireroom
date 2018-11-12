656958150f7da09304e7b1a92a902b7e5301a64c
ryan-muraliraj <ryan_muraliraj@myabcusd.org>
2018-11-12 12:20:40 -0800

    official update

---
+++ Fire Room/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Fire_Room
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(0, 2);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1178, 655);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 670);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1020, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1032, 672);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(452, 758);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(446, 38);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(921, 751);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(246, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Set Nickname";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 758);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Currently known as: Anonymous";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 833);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Fire Room/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fire_Room
+{
+    public partial class Form1 : Form
+    {
+        private string permanante;
+        private string username = "Anonymous";
+        private int original;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            original = 0;
+        }
+
+        public void sendMessage(object sender)
+        {
+            DateTime date = DateTime.Now;
+            string sdate = date.ToString("yyyy:MM:dd");
+            string stime = date.ToString("HH:mm:ss");
+            string completevalue = username + ": " + textBox1.Text.ToString();
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                Name = stime,
+                Value = completevalue,
+                
+            });
+            var request = WebRequest.CreateHttp("https://testing-firebase-60b9b.firebaseio.com/.json");
+
+            request.Method = "PATCH";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            var response = request.GetResponse();
+            json = (new StreamReader(response.GetResponseStream())).ReadToEnd();
+
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendMessage(sender);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string URL = "https://testing-firebase-60b9b.firebaseio.com/.json";
+            HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(URL);
+            request1.ContentType = "application/json: charset-utf-8";
+            HttpWebResponse response1 = request1.GetResponse() as HttpWebResponse;
+            Stream responsestream = response1.GetResponseStream();
+            StreamReader Read = new StreamReader(responsestream, Encoding.UTF8);
+            var output = Read.ReadToEnd();
+            string outphrase = output;
+            string[] outie = outphrase.Split('"');
+
+            if (permanante == outie[7])
+            {
+            }
+            else
+            {
+                permanante = outie[7];
+                listBox1.Items.Add(permanante);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            username = textBox2.Text;
+            label1.Text = "Currently known as: " + username;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+            {
+                sendMessage(sender);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                sendMessage(sender);
+            }
+        }
+    }
+}
